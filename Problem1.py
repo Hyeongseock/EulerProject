7ab8b1120f994ddeca467ead3724adbff541c513
Hyeongseock <killerfx@hanmail.net>
2018-07-02 22:54:01 +0900

    Update and rename Problem1.txt to Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -1,9 +1,13 @@
+'''
 Multiples of 3 and 5
 Problem 1
+'''
 
+'''
 English version
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
+'''
 
 #Setting 
 three_list = []
@@ -37,17 +41,17 @@ for i in all_list:
 #answer
 print(sum)
 
-time spent : 0.0009975433349609375 seconds
+#time spent : 0.0009975433349609375 seconds
 
 
-====================================================================================
-====================================================================================
-====================================================================================
 
+
+
+'''
 Korean version
 10보다 작은 자연수 중에서 3 또는 5의 배수는 3, 5, 6, 9 이고, 이것을 모두 더하면 23입니다.
 1000보다 작은 자연수 중에서 3 또는 5의 배수를 모두 더하면 얼마일까요?
-
+'''
 
 #초기값 셋팅
 three_list = []
@@ -81,4 +85,4 @@ for i in all_list:
 #정답
 print(sum)
 
-계산에 걸린 시간 : 0.0009975433349609375 seconds
+#계산에 걸린 시간 : 0.0009975433349609375 seconds
